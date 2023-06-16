864557d834a8bb42a1c9b42b4a1200d411ed34b7
Adrien Rossato <adrien.marcos@gmail.com>
2023-06-16 14:00:32 -0300

    feat: create Questions and Result components

---
+++ src/components/Result.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="result">
+    <div class="title">You got sample result 1!</div>
+    <div class="desc">Enter a short description here about the result.</div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'result',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style></style>
